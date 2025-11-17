764c1bcddbea9cb16eba79f051c628b26c04e6fd
Ildi Dvorani <dvoranii@ildidev.com>
2025-11-17 16:10:22 -0500

    updated vercel.json and vit config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Add this line for Vercel deployment
+  base: "/", // Change this from './' to '/'
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+  },
 });
